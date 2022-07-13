c0795ea4b3c6c92667adc506a81cd24141aa91cb
Anastasia <nekoluchka91@gmail.com>
2022-07-13 15:13:32 +0400

    Add Task_5

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, 
+// которая будет выдавать название дня недели 
+// по заданному номеру.
+// 3 -> среда
+// 5 -> пятница
+
+Console.Write("Введите номер дня недели: ");
+int day_week = Convert.ToInt32(Console.ReadLine());
+
+if (day_week == 1) 
+{
+Console.WriteLine("Monday");
+} 
+else if (day_week == 2) 
+{
+Console.WriteLine("Tuesday"); 
+} 
+else if (day_week == 3) 
+{
+Console.WriteLine("Wednesday");
+} 
+else if (day_week == 4) 
+{
+Console.WriteLine("Thursday");
+} 
+else if (day_week == 5) 
+{
+Console.WriteLine("Friday");
+} 
+else if (day_week == 6) 
+{
+Console.WriteLine("Saturday");
+} 
+else if (day_week == 7) 
+{
+Console.WriteLine("Sunday");
+} 
+else 
+{
+    Console.WriteLine("Error");
+}  
