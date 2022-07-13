8d7ac248fcd5782fd4ac7c09b2af36f2a30c0fe0
Anastasia <nekoluchka91@gmail.com>
2022-07-13 23:03:20 +0400

    Add Homework_Task_6

--- Homework_Task_4/Program.cs
+++ Homework_Task_4/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая 
+// принимает на вход три числа и
+// выдает максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44, 5, 78 -> 78
+// 22, 3, 9 -> 22
+
+
+// int number_a = x;
+// int number_b = y;
+// int number_c = z;
+
+// int max = number_a;
+
+// if (number_a > max) max = number_a;
+// if (number_b > max) max = number_b;
+// if (number_c > max) max = number_c;
+
+// Console.Write("max = ");
+// Console.WriteLine(max);
+
+int number_a = 98;
+int number_b = 50;
+int number_c = 270;
+
+int max = number_a;
+
+if (number_a > max) max = number_a;
+if (number_b > max) max = number_b;
+if (number_c > max) max = number_c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
