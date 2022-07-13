00e6b34bba64e6a0f660955619604def52c4a2d9
Anastasia <nekoluchka91@gmail.com>
2022-07-13 23:26:55 +0400

    Add Homework_Task_8

--- Homework_Task_6/Program.cs
+++ Homework_Task_6/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая 
+// на вход принимает число и
+// выдает, является ли число четным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// if (a % 2 == 0)
+// {
+// Console.WriteLine("yes");
+// }
+// else
+// {
+// Console.WriteLine("no");
+// }
+
+
+if (3 % 2 == 0)
+{
+Console.WriteLine("yes");
+}
+else
+{
+Console.WriteLine("no");
+}
