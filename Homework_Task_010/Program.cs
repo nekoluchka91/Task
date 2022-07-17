861a2c03ed74c4b5b765d7a02dcc38a076fa4584
Anastasia <nekoluchka91@gmail.com>
2022-07-17 14:27:50 +0400

    Add HW_Task_013

--- Homework_Task_010/Program.cs
+++ Homework_Task_010/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает
+// на вход трехзначное число и
+// на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите целое трехначное число: "); // 456
+int numA = Convert.ToInt32(Console.ReadLine());
+int numB = numA / 10; // 456 / 10 = 45
+int result = numB % 10; // 45 % 10 = 5
+Console.WriteLine(result);
