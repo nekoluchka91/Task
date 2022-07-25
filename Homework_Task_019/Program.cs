8403036e20b51df8d7235548de6181a47ef17ab8
Anastasia <nekoluchka91@gmail.com>
2022-07-25 13:04:47 +0400

    Task_19_Homework

--- Homework_Task_019/Program.cs
+++ Homework_Task_019/Program.cs
@@ -10,13 +10,32 @@
 
 Console.WriteLine("Введите пятизначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-
-void Check(num collection);
+int remainder; // remaider - остаток
+int temp = num;
+int sum = 0;
+while (num > 0) // цикл, пока число больше 0
+{ 
+    remainder = num % 10; // ищем остаток (после запятой)
+    sum = sum * 10 + remainder; // умножаем sum на 10 и добавляем остаток
+    num = num / 10; // ищем частное от деление (до запятой)
+}
+if (temp == sum)
 {
-    if (num[0] == num[4] || num[1] == num[3]) "Введенное число является палиндромом.";
-    return "Введенное число не является палиндромом.";
+    Console.WriteLine($"Данное число является палиндромом.");
+}
+else
+{
+    Console.WriteLine($"Данное число не является палиндромом.");
 }
 
-result = Check(num[index]collection);
-Console.WriteLine(result);
+
+
+// void Check(num collection);
+// {
+//     if (num[0] == num[4] || num[1] == num[3]) "Введенное число является палиндромом.";
+//     return "Введенное число не является палиндромом.";
+// }
+
+// result = Check(num[index]collection);
+// Console.WriteLine(result);
 
