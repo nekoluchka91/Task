fb7771f2af1796f4a009e9e4c7bb2589ff46c183
Anastasia <nekoluchka91@gmail.com>
2022-08-04 11:44:27 +0400

    Add Homework_Task_25_27_29

--- Homework_Task_025/Program.cs
+++ Homework_Task_025/Program.cs
@@ -1,4 +1,12 @@
 ﻿// Напишите цикл, который принимает на вход два числа (А и В) и
-// возводит числа А в натуральную степень В.
+// возводит число А в натуральную степень В.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16 (2⁴)
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Число a в степени b = " + Math.Pow(a, b));
--- Homework_Task_027/Program.cs
+++ Homework_Task_027/Program.cs
@@ -4,3 +4,19 @@
 // 82 -> 10
 // 9012 -> 12
 
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int GetSum(int number)
+{
+    int sum = 0;
+    while (n > 0)
+    {
+    number = n % 10;
+    n = n / 10;
+    sum = sum + number;
+}
+return sum;
+}
+int res = GetSum(n);
+Console.WriteLine($"Сумма цифр в заданном числе = {res}");
